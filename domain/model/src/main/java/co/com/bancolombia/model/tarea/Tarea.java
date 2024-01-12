e97d5f16a8da15cf48edb72a33fc69a1dec65d3e
DanielChambueta08 <drosorio@bancolombia.com.co>
2024-01-11 20:34:20 -0500

    [ADD] Entity Tarea and TareaRepository in Model

---
+++ domain/model/src/main/java/co/com/bancolombia/model/tarea/Tarea.java
@@ -0,0 +1,18 @@
+package co.com.bancolombia.model.tarea;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class Tarea {
+    private Long id;
+    private String titulo;
+    private String descripcion;
+    private String estado;
+
+    public static Tarea newTarea(Long id, String titulo, String descripcion, String estado) {
+        return new Tarea(id, titulo, descripcion, estado);
+    }
+}
